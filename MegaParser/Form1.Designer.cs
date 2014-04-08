248893c09b547586c69e9518131edbb72bbfbb22
Quantumfox <donpaul@mail.ru>
2014-04-08 21:14:42 +0400

    Release
    First release. Downloads statistics from Megafon site account to Excel
    file

---
+++ MegaParser/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace FAMassDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PasswordMaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.CapchaText = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.CapchaPic = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.CapchaPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Телефон";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пароль";
+            // 
+            // PasswordMaskedTextBox
+            // 
+            this.PasswordMaskedTextBox.Location = new System.Drawing.Point(100, 35);
+            this.PasswordMaskedTextBox.Name = "PasswordMaskedTextBox";
+            this.PasswordMaskedTextBox.PasswordChar = '*';
+            this.PasswordMaskedTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PasswordMaskedTextBox.TabIndex = 2;
+            this.PasswordMaskedTextBox.UseSystemPasswordChar = true;
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Location = new System.Drawing.Point(100, 2);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(100, 20);
+            this.LoginTextBox.TabIndex = 1;
+            // 
+            // CapchaText
+            // 
+            this.CapchaText.Location = new System.Drawing.Point(99, 122);
+            this.CapchaText.Name = "CapchaText";
+            this.CapchaText.Size = new System.Drawing.Size(100, 20);
+            this.CapchaText.TabIndex = 3;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(99, 148);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(128, 23);
+            this.StartButton.TabIndex = 4;
+            this.StartButton.Text = "Начать скачивание";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // CapchaPic
+            // 
+            this.CapchaPic.Location = new System.Drawing.Point(15, 61);
+            this.CapchaPic.Name = "CapchaPic";
+            this.CapchaPic.Size = new System.Drawing.Size(265, 50);
+            this.CapchaPic.TabIndex = 9;
+            this.CapchaPic.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Защитный код";
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Location = new System.Drawing.Point(205, 122);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(75, 23);
+            this.RefreshButton.TabIndex = 11;
+            this.RefreshButton.Text = "Обновить";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3600000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 190);
+            this.Controls.Add(this.RefreshButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CapchaPic);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.CapchaText);
+            this.Controls.Add(this.LoginTextBox);
+            this.Controls.Add(this.PasswordMaskedTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MegaParser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CapchaPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox PasswordMaskedTextBox;
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.TextBox CapchaText;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.PictureBox CapchaPic;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MegaParser/Form1.cs
@@ -0,0 +1,240 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace FAMassDownloader
+{
+
+    public partial class Form1 : Form
+    {
+        delegate void DownloadProcess(string Login, string Password, string Link);
+
+
+        MyWebClient client = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            client = new MyWebClient();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+   
+
+            if (LoginTextBox.Text != "" && PasswordMaskedTextBox.Text != "")
+            {
+                if (CapchaText.Text != "")
+                {
+                    if (client.TryToLogin(LoginTextBox.Text, PasswordMaskedTextBox.Text, CapchaText.Text))
+                    {
+
+                       
+                        if (client.Gotopayments())
+                        {
+                            client.GetTableData();
+                            timer1.Start();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Сессия была завершена! Пожалуйста, обновите защитный код и авторизуйтесь снова!");
+                            timer1.Stop();
+                        }
+                    }
+                    else
+                    {
+                        timer1.Stop();
+                        CapchaPic.ImageLocation = client.GetCapcha();  
+                    }
+                }
+                else MessageBox.Show("Укажите секретное слово!", "Проблема!");
+
+            }
+            else
+            {
+                MessageBox.Show("Укажите свои логин и пароль!", "Проблема!");
+            }
+
+
+        }
+
+
+       
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            CapchaPic.ImageLocation = client.GetCapcha();  
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CapchaPic.ImageLocation = client.GetCapcha();
+           
+        }
+        public class MyWebClient
+        {
+           System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+           WebBrowser WB = null;
+           public MyWebClient()
+            {
+                WB = new WebBrowser();
+              WB.ScriptErrorsSuppressed = true;
+                         }
+            System.Windows.Forms.HtmlDocument LoadPage(string url)
+            {
+                WB.Navigate( url);
+                while (WB.ReadyState != WebBrowserReadyState.Complete)
+                    System.Windows.Forms.Application.DoEvents();
+                    System.Windows.Forms.HtmlDocument doc = WB.Document;
+                return doc;
+            }
+
+           public string GetCapcha()
+            {
+               System.Windows.Forms.HtmlDocument doc = LoadPage( "https://volgasg.megafon.ru/");
+               HtmlElementCollection collection = doc.Images;
+               foreach (HtmlElement a in collection ){
+                   if (a.GetAttribute("id") == "cryptogram" ){
+                       return a.GetAttribute("src");
+                   }
+               }
+                return null;
+            }
+
+           public bool TryToLogin(string Login, string Password, string Capcha)
+           {
+               WB.Document.GetElementById("LOGIN").InnerText = Login;
+               WB.Document.GetElementById("passwdId").InnerText = Password;
+               WB.Document.GetElementById("codeId").InnerText = Capcha;
+               WB.Document.GetElementById("submitBtnId").InvokeMember("click");
+               while (WB.ReadyState != WebBrowserReadyState.Complete)
+                   System.Windows.Forms.Application.DoEvents();
+               HtmlAgilityPack.HtmlDocument HD = new HtmlAgilityPack.HtmlDocument();
+
+               HD.LoadHtml(((dynamic)WB.Document.DomDocument).documentElement.outerHTML);
+
+
+               HtmlAgilityPack.HtmlNodeCollection messages = HD.DocumentNode.SelectNodes("//div[@id='divMessage']/div/div/div | //div[@id='passwdId-ErrorMessage']");
+              if(messages != null){
+                  foreach(HtmlNode n in messages){
+                       switch(n.InnerText){
+                           case "Введен неверный защитный код.":
+                               MessageBox.Show(n.InnerText);
+                               return false;
+                           break;
+                           case "Указан неправильный пароль.":
+                               MessageBox.Show(n.InnerText);
+                               return false;
+                           break;
+
+                       }
+                     
+                  }
+                 
+                  return true;
+              }
+                
+               return true;
+
+           }
+
+           public bool Gotopayments()
+           {
+
+               while (WB.Document.GetElementById("ACCOUNT_INFO") == null && WB.Document.GetElementById("PAYMENTS_INFO") == null) { Application.DoEvents(); Thread.Sleep(200); }
+               if (WB.Document.GetElementById("menu_oper_id_PAYMENTS_INFO") != null || WB.Document.GetElementById("PAYMENTS_INFO") != null)
+               {
+                   WB.Document.GetElementById("menu_oper_id_PAYMENTS_INFO").InvokeMember("click");
+                   return true;
+               }
+               else
+                   return false;
+
+           }
+
+           public void GetTableData()
+           {
+               while (WB.Document.GetElementById("div_payments") == null) { Application.DoEvents(); Thread.Sleep(200); }
+               HtmlAgilityPack.HtmlDocument HD = new HtmlAgilityPack.HtmlDocument();
+
+               HD.LoadHtml(((dynamic)WB.Document.DomDocument).documentElement.outerHTML);
+              
+
+               HtmlAgilityPack.HtmlNodeCollection table = HD.DocumentNode.SelectNodes("//div[@id='div_payments']/table/tbody/tr");
+
+               Excel.Application app = new Excel.Application();
+               app.Workbooks.Add();            
+               Excel.Workbook book = app.ActiveWorkbook;
+               Excel.Worksheet sheet = book.Worksheets.get_Item(1);
+               int row = 1;
+               int column = 1;
+               if (table != null)
+               {
+                   foreach (HtmlAgilityPack.HtmlNode node in table)
+                   {
+                       HtmlAgilityPack.HtmlNodeCollection cells = node.SelectNodes("td");
+                       foreach (HtmlAgilityPack.HtmlNode cell in cells)
+                       {
+                           HtmlAgilityPack.HtmlNodeCollection strings = cell.SelectNodes("div");
+                           if (strings.Count > 1)
+                           {
+                               string result = "";
+                               foreach (HtmlAgilityPack.HtmlNode a in strings)
+                               {
+                                   result += a.InnerText+" ";
+                               }
+                               sheet.Cells[row, column] = result;
+
+                           }
+                           else sheet.Cells[row, column] = strings[0].InnerText;
+
+
+                           column++;
+                       }
+                       column = 1;
+                       row++;
+                   }
+
+
+
+               }
+
+
+             
+               app.DisplayAlerts = false;
+               book.SaveAs(  Application.StartupPath+"/Data",  Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlShared ,  Microsoft.Office.Interop.Excel.XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+               book.Close();
+               app.Quit();
+           }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client.Gotopayments();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (client.Gotopayments())
+            {
+                client.GetTableData();
+            }
+            else
+            {
+                MessageBox.Show("Сессия была завершена! Пожалуйста, обновите защитный код и авторизуйтесь снова!");
+            }
+        }
+    }
+}
